0a794fd895baa65957f109b40c1252e19a384391
kanchankumari07 <98989464+kanchankumari07@users.noreply.github.com>
2022-12-01 07:53:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,61 @@ let myLogger = (req, res, next) => {
   next();
 };
 
+
 app.use(express.static('public'));
+
+let movies = [
+  {
+    "Title":"3 idoits",
+    "Director":"Rajkumar Hirani",
+    "Genre":"Drama",
+  },
+  {
+    "Title":"Gladiator",
+    "Director":"Ridley Scott",
+    "Genre":"Action",
+  },
+  {
+    "Title":"IT",
+    "Director":"andres Muschietti",
+    "Genre":"Horror",
+  },
+
+  { "Title":"The Notebook",
+  "Director":"Nick Cassavetes",
+  "Genre":"Romance",
+ },
+
+  { "Title":"Mission Mangal",
+  "Director":"Jagan Shakti",
+  "Genre":"Science Fiction",
+},
+{
+  "Title":"Encanto",
+  "Director":"Byron Howard,Jared Bush",
+  "Genre":"Fantasy",
+},
+{
+  "Title":"Shutter Island",
+  "Director":"Martin Scorsese",
+  "Genre":"Thriller",
+},
+{
+  "Title":"Cruella",
+  "Director":"Craig Gillespie",
+  "Genre":"Comedy",
+},
+{
+  "Title":"Moana",
+  "Director":"Ron clements,John Musker",
+  "Genre":"Adventure",
+},
+{
+"Title":"English vinglish",
+"Director":"Gauri Shine",
+"Genre":"Comedy",
+},
+]
 
 app.get('/', (req, res) => {
   res.send('Welcome to my top movies list!');
